df4432f4cfd35dc4ddb13b991217982464f6ef28
iasmyn7 <116596914+iasmyn7@users.noreply.github.com>
2024-03-20 10:59:58 -0300

    20

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const botoes=document.querySelectorAll(".botao");
+const botoes=document.querySelectorAll(".botao");// variavel que armazena todos os itens com a classe botão 
 
 
-for(let i=0;i) <botoes.length;i++){
-    botoes[i] .onclick = function(){
+for(let i=0;i) <botoes.length;i++){// lopp que executa enquanto j<quantidade de botoes
 
-        for(let j= 0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
+    botoes[i] .onclick = function(){ //cria uma função
+
+        for(let j= 0;j<botoes.length;j++){//lopp que executa enquanto j<quantidade de botoes
+            botoes[j].classList.remove("ativo"); //remove a palavra da classe.
         }
         
-        botoes[i].classList.add("ativo");
+        botoes[i].classList.add("ativo");//adicina a palavra ativo da classe.
     }
     console.log();
 }
