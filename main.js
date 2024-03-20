a2cf9065812caaeddb8f1c96f6561de05cb3538f
iasmyn7 <116596914+iasmyn7@users.noreply.github.com>
2024-03-20 11:49:21 -0300

    20

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const botoes=document.querySelectorAll(".botao");// variavel que armazena todos 
 
 for(let i = 0; i <botoes.length; i++) {// lopp que executa enquanto j<quantidade de botoes
 
-    botoes[i] .onclick = function () { //cria uma função
+    botoes[i].onclick = function () { //cria uma função
 
         for(let j = 0;j< botoes.length; j++) { //lopp que executa enquanto j<quantidade de botoes
             botoes[j].classList.remove("ativo"); //remove a palavra da classe.
