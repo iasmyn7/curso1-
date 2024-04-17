d5bfdfbe5976a80f7390ce1f756176fe20fe53c9
iasmyn7 <116596914+iasmyn7@users.noreply.github.com>
2024-04-17 10:35:25 -0300

    17

--- main.js
+++ main.js
@@ -1,15 +1,59 @@
-const botoes=document.querySelectorAll(".botao");// variavel que armazena todos os itens com a classe botão 
-const botoes=document.querySelectorAll(".aba-conteudo")//variavel que armazena todos os itens com a classe aba conteudo
 
-for(let i = 0; i <botoes.length; i++) {// lopp que executa enquanto j<quantidade de botoes
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-    botoes[i].onclick = function () { //cria uma função
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () { // cria uma função
 
-        for(let j = 0;j< botoes.length; j++) { //lopp que executa enquanto j<quantidade de botoes
-            botoes[j].classList.remove("ativo"); //remove a palavra da classe.
+        for (let j = 0; j < botoes.length; j++) { //loop que executa enquanto j<quantidade de bot
+            botoes[j].classList.remove("ativo"); //remove a palavra ativo da classe botoes
+            textos[j].classList.remove("ativo"); //remove a palavra ativo da classe textos
         }
-        botoes[i].classList.add("ativo");//adicina a palavra ativo da classe.
-        textos[i].classList.add("ativo");//adicina a palavra ativo da classe.
+
+        botoes[i].classList.add("ativo"); // adiciona a palavra ativo da classe botoes
+        textos[i].classList.add("ativo"); // adiciona a palavra ativo da classe textos
     }
-    console.log();
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
+    } else {
+        return [0,0,0,0];
+    }
+}
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){ //loop que executa enquanto i<quantidade de bot
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
